2c496b73f9d56cb2c6a43e3293d0dbbf29627359
Aadi <mac@Aadis-Macbook-Pro.local>
2019-11-06 19:34:47 -0500

    bruh

--- app/src/main/java/ca/team2706/giorno/MainActivity.java
+++ app/src/main/java/ca/team2706/giorno/MainActivity.java
@@ -2,7 +2,9 @@ package ca.team2706.giorno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void syncActivity(View view) {
+        startActivity(new Intent(this, SyncActivity.class));
     }
 
 }
