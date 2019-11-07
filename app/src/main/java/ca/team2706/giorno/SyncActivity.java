8a923fc230f35f3e45c54de89c8e71563888afcc
Alden Peeters <alden.peeters@leagueh.xyz>
2019-11-06 19:02:32 -0500

    Added empty SyncActivity

---
+++ app/src/main/java/ca/team2706/giorno/SyncActivity.java
@@ -0,0 +1,14 @@
+package ca.team2706.giorno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SyncActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sync);
+	}
+}
