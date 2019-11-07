1738141ed2d285b81bf6ffee38886a58bcd834f4
Alden Peeters <alden.peeters@leagueh.xyz>
2019-11-06 19:16:00 -0500

    Added debug stuff to the empty sync activity

--- app/src/main/java/ca/team2706/giorno/SyncActivity.java
+++ app/src/main/java/ca/team2706/giorno/SyncActivity.java
@@ -1,14 +1,29 @@
 package ca.team2706.giorno;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import ca.team2706.giorno.sync.IPSync;
+import ca.team2706.giorno.sync.SyncManager;
+
 public class SyncActivity extends AppCompatActivity {
+
+	private SyncManager syncManager;
+	private IPSync sync;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sync);
+		syncManager = new SyncManager();
+		sync = new IPSync();
+		sync.startSync(getApplicationContext(), syncManager);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		sync.stopSync();
 	}
 }
